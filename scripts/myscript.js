75854e80c9a506d581aea9d52fd36d5a8c7e2071
ssevtaparslan <sevtaparslan37@gmail.com>
2025-05-10 17:43:38 +0300

    Delete scripts directory

--- scripts/myscript.js
+++
@@ -1,7 +0,0 @@
-function sepeteEkle(ad, fiyat) {
-    let sepet = JSON.parse(localStorage.getItem("sepet")) || [];
-    sepet.push({ ad, fiyat });
-    localStorage.setItem("sepet", JSON.stringify(sepet));
-    window.location.href = "sepet.html";
-}
-
